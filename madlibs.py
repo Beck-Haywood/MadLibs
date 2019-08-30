5e2f31cbbb9deeba2c0b4caa82d3e6b9d90194a1
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-08-30 12:14:42 -0700

    Wrote most of the madlib and implemented some data from the lists

--- madlibs.py
+++ madlibs.py
@@ -16,6 +16,7 @@ class bcolors:
 adjectives = list()
 nouns = list()
 verbs = list()
+places = list()
 #Function asks for nouns and puts them in a list
 def add_noun_list():
     runtime = 0
@@ -37,6 +38,13 @@ def add_verb_list():
         adj = input("Enter a verb! ")
         verbs.append(adj)
         runtime += 1
+#Function asks for places and puts them in a list
+def add_place_list():
+    runtime = 0
+    while runtime !=3:
+        adj = input("Enter a place! ")
+        places.append(adj)
+        runtime += 1
 #Funtion lists items to test its working
 def list_all_items():
     for list_item in nouns,adjectives,verbs:
@@ -45,5 +53,10 @@ def list_all_items():
 add_noun_list()
 add_adj_list()
 add_verb_list()
+add_place_list()
 
 list_all_items()
+
+print("A long time ago in a " + places[0] + " far, far away... the " + adjectives[0] +  " evil coperation " + nouns[0] + " tech developed a " + nouns[1]
++ ". This " + nouns[1] + " forced the users to repeatedly " + verbs[0] + ". This Angered a user of the product named " + nouns[2]
++ ", this user was skilled in the art of " + verbs[1] + "(ing) this allowed " + nouns[2] + " to face")
